2aee697b935c49bccc65e0e35c00cb73057051b3
Guilherme Ferrari <guiferrari10@hotmail.com>
2019-10-30 22:47:44 -0300

    Criado envio de email ao cancelar um agendamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '83acaa87674d80',
+    pass: '7cecc68c1a75b0'
+  },
+  default: {
+    from: 'Guilherme Ferrari <noreply@goferrari.com>'
+  }
+}
